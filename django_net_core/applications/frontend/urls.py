bc9b063f41faa696b530843bdc3bf5da39a8fb4b
Neolize <rumaria.work@gmail.com>
2023-05-21 13:10:20 +0300

    Added user profile and edit user profile views

--- django_net_core/applications/frontend/urls.py
+++ django_net_core/applications/frontend/urls.py
@@ -5,6 +5,8 @@ from applications.frontend import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('edit_profile', views.EditUserProfileView.as_view(), name='edit_profile'),
     path('users/', views.UsersView.as_view(), name='users'),
 
     path('login/', views.LoginUserView.as_view(), name='login'),
--- django_net_core/applications/frontend/views.py
+++ django_net_core/applications/frontend/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, View, CreateView
+from django.views.generic import ListView, CreateView, View
 from django.contrib.auth.views import LoginView
 from django.core.handlers.wsgi import WSGIRequest
 
-from allauth.account.views import SignupView, LogoutView
-from allauth.account.forms import SignupForm
+from allauth.account.views import LogoutView
 
 from applications.user_profiles import forms
 from applications.user_profiles.services.crud import read, update
@@ -24,6 +23,22 @@ class UsersView(ListView):
     context_object_name = 'users'
 
 
+class SignupUserView(CreateView):
+    template_name = 'account/signup.html'
+    form_class = forms.SignupUserForm
+
+    def form_valid(self, form: forms.SignupUserForm):
+        print("Form valid")
+        print(dir(form))
+        print(form.cleaned_data)
+        return render(self.request, self.template_name, context={'form': self.form_class(self.request.POST)})
+        # return redirect(to='signup')
+        # return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
 class LoginUserView(LoginView):
     template_name = 'account/login.html'
     form_class = forms.LoginUserForm
@@ -36,38 +51,23 @@ class LoginUserView(LoginView):
         return reverse_lazy('home')
 
 
-class SignupUserView(CreateView):
-    template_name = 'account/signup.html'
-    form_class = forms.SignupUserForm
-
-    def get_success_url(self):
-        return reverse_lazy('home')
-
-    # def get(self, request: WSGIRequest):
-    #     context = {
-    #         'form': self.form_class()
-    #     }
-    #     print(self.form_class().fields)
-    #     return render(request, self.template_name, context=context)
-
-
-# class SignupUserViewTest(SignupView):
-#     form_class = forms.SignupUserFormTest
-#
-#     def form_valid(self, form: SignupForm):
-#         print(dir(form))
-#         print(form.cleaned_data)
-#         form.add_error(None, 'This is non fields error')
-#         return render(self.request, self.template_name, context={'form': form})
-#         # return redirect(to='signup')
-#
-#     # def get_authenticated_redirect_url(self):
-#     #     return reverse_lazy('home')
-#
-#     def get_success_url(self):
-#         return reverse_lazy('home')
-
-
 class LogoutUserView(LogoutView):
     def get_redirect_url(self):
         return reverse_lazy('login')
+
+
+class UserProfileView(View):
+    template = 'user_profiles/detail/profile.html'
+
+    def get(self, request: WSGIRequest):
+        context = {
+            'profile_content': 'User profile',
+        }
+        return render(request, self.template, context=context)
+
+
+class EditUserProfileView(View):
+    template = 'user_profiles/detail/edit.html'
+
+    def get(self, request: WSGIRequest):
+        return render(request, self.template)
