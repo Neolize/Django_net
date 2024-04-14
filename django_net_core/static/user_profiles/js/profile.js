ff44e9ffc62c1060fedf72ca55b8e97869f288fd
Neolize <rumaria.work@gmail.com>
2024-04-14 14:01:19 +0300

    Fixed pagination breakdown. Now pagination works properly every time user clicks on different page numbers.

--- django_net_core/static/user_profiles/js/profile.js
+++ django_net_core/static/user_profiles/js/profile.js
@@ -3,6 +3,7 @@
 
 function main() {
     scrollToPosts();
+    addURLParametersToPagination();
 }
 
 
@@ -34,6 +35,22 @@ function deletePost(event) {
     const answer = confirm('Are you sure you want to delete this post?');
     if (answer === false) {
         event.preventDefault();
+    }
+}
+
+/**
+ *  Function adds 'posts' parameter to a URL query string for all pagination pages
+ *  if  this page has 'posts' parameter set in a URL querystring.
+ */
+function addURLParametersToPagination() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const postsToShow = urlParams.get('posts');
+
+    if(postsToShow) {
+        const allLinkElements = document.querySelectorAll('[data-posts]')
+        for (let link of allLinkElements) {
+            link.href = `${link.href}&posts=${postsToShow}`;
+        }   
     }
 }
 
