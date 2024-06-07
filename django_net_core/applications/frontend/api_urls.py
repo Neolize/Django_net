11f18d0a2fdd18b5fa551dbd7acc28fd20708adc
Neolize <rumaria.work@gmail.com>
2024-06-07 23:32:22 +0300

    Changed API urls.

--- django_net_core/applications/frontend/api_urls.py
+++ django_net_core/applications/frontend/api_urls.py
@@ -6,20 +6,20 @@ from applications.groups import views as g_views
 
 urlpatterns = [
     path('user/<int:pk>/', up_views.PublicUserDetailAPIView.as_view(), name='user_public_api'),
-    path('user_list/', up_views.PublicUserListAPIView.as_view(), name='user_list_public_api'),
+    path('user-list/', up_views.PublicUserListAPIView.as_view(), name='user_list_public_api'),
 
     path('group/<str:group_slug>/', g_views.PublicGroupDetailAPIView.as_view(), name='group_public_api'),
-    path('group_list/', g_views.PublicGroupListAPIView.as_view(), name='group_list_public_api'),
+    path('group-list/', g_views.PublicGroupListAPIView.as_view(), name='group_list_public_api'),
     path(
-        'group_post/<str:group_post_slug>/',
+        'group-post/<str:group_post_slug>/',
         g_views.PublicGroupPostDetailAPIView.as_view(),
         name='group_post_public_api'
     ),
     path(
-        'group_post_comments/<str:group_post_slug>/',
+        'group-post-comments/<str:group_post_slug>/',
         g_views.PublicGroupPostCommentListAPIView.as_view(),
         name='group_post_comment_public_api'
     ),
-    path('alter_group/', g_views.AlterGroupAPIView.as_view()),
-    path('alter_group/<str:group_slug>/', g_views.AlterGroupAPIView.as_view()),
+    path('group-alteration/', g_views.AlterGroupAPIView.as_view()),
+    path('group-alteration/<str:group_slug>/', g_views.AlterGroupAPIView.as_view()),
 ]
